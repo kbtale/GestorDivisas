f369fb32a235197a4c593ff7fa8aec3913b06ff9
Carlos Bolívar <cebemind@hotmail.com>
2023-02-27 00:39:47 -0400

    3

--- views.py
+++ views.py
@@ -20,6 +20,13 @@ def showdata(request):
     currencies = [
         {
             "name": "USD",
+            "icon": """
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-coin" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                       <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+                       <path d="M14.8 9a2 2 0 0 0 -1.8 -1h-2a2 2 0 1 0 0 4h2a2 2 0 1 1 0 4h-2a2 2 0 0 1 -1.8 -1"></path>
+                       <path d="M12 7v10"></path>
+                    </svg>""",
             "sources": [
                 {
                     "name": sources[0],
@@ -37,6 +44,15 @@ def showdata(request):
         },
         {
             "name": "EUR",
+            "icon": """
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-coin-euro" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+               <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+               <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+               <path d="M14.401 8c-.669 -.628 -1.5 -1 -2.401 -1c-2.21 0 -4 2.239 -4 5s1.79 5 4 5c.9 0 1.731 -.372 2.4 -1"></path>
+               <path d="M7 10.5h4"></path>
+               <path d="M7 13.5h4"></path>
+            </svg>
+            """,
             "sources": [
                 {
                     "name": sources[0],
